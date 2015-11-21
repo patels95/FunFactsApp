f283f2ae17191455c718f280caa0337da516e4d1
patels95 <patels326@gmail.com>
2015-11-21 16:55:26 -0500

    savedInstanceState orientation fix

--- src/com/example/funfacts/FunFactsActivity.java
+++ src/com/example/funfacts/FunFactsActivity.java
@@ -17,10 +17,18 @@ import android.widget.Toast;
 
 public class FunFactsActivity extends Activity {
 	
-	public static final String TAG = FunFactsActivity.class.getSimpleName(); 
-	
+	public static final String TAG = FunFactsActivity.class.getSimpleName();
+	private static final String KEY_FACT = "KEY_FACT";
+	private static final String KEY_COLOR = "KEY_COLOR";
+
 	private FactBook mFactBook = new FactBook();
 	private ColorWheel mColorWheel = new ColorWheel();
+	private String mFact = mFactBook.mfacts[0];
+	private int mColor = Color.parseColor(mColorWheel.mColors[0]);
+
+	private TextView mFactLabel;
+	private Button mShowFactButton;
+	private RelativeLayout mRelativeLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -28,25 +36,42 @@ public class FunFactsActivity extends Activity {
 		setContentView(R.layout.activity_fun_facts);
 		
 		//Declare View variables and assign them the Views from the layout file
-		final TextView factLabel = (TextView) findViewById(R.id.factTextView);
-		final Button showFactButton = (Button) findViewById(R.id.showFactButton);
-		final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
+		mFactLabel = (TextView) findViewById(R.id.factTextView);
+		mShowFactButton = (Button) findViewById(R.id.showFactButton);
+		mRelativeLayout = (RelativeLayout) findViewById(R.id.relativeLayout);
 		
 		View.OnClickListener listener = new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				String fact = mFactBook.getFact();
-				int color = mColorWheel.getColor();
-				factLabel.setText(fact);
-				relativeLayout.setBackgroundColor(color);
-				showFactButton.setTextColor(color);
+				mFact = mFactBook.getFact();
+				mColor = mColorWheel.getColor();
+				mFactLabel.setText(mFact);
+				mRelativeLayout.setBackgroundColor(mColor);
+				mShowFactButton.setTextColor(mColor);
 				
 			}
 		};
-		showFactButton.setOnClickListener(listener);
-		
-		
-		
-		
+		mShowFactButton.setOnClickListener(listener);
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+
+		outState.putString(KEY_FACT, mFact);
+		outState.putInt(KEY_COLOR, mColor);
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+
+		mFact = savedInstanceState.getString(KEY_FACT);
+		mColor = savedInstanceState.getInt(KEY_COLOR);
+
+		// update view
+		mFactLabel.setText(mFact);
+		mRelativeLayout.setBackgroundColor(mColor);
+		mShowFactButton.setTextColor(mColor);
 	}
 }
